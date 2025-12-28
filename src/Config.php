d560dc08fa67f6dd20dd44ac71ee4b662921392e
nydia87 <349196713@qq.com>
2025-12-28 23:18:08 +0800

    Config类调整

--- src/Config.php
+++ src/Config.php
@@ -14,12 +14,6 @@ class Config
 	protected static $config = [];
 
 	/**
-	 * 配置前缀
-	 * @var string
-	 */
-	protected static $prefix = 'colaphp:config';
-
-	/**
 	 * @param string $file 配置文件名
 	 * @param mixed $name
 	 * @return mixed
@@ -32,7 +26,6 @@ class Config
 				return self::set($name, include $file);
 			}
 		}
-
 		return self::$config;
 	}
 
@@ -44,19 +37,17 @@ class Config
 	 */
 	public static function get($name = null, $default = null)
 	{
-		if ($name && strpos($name, '.') === false) {
-			$name = self::$prefix . '.' . $name;
-		}
-
 		// 无参数时获取所有
 		if (empty($name)) {
 			return self::$config;
 		}
-
+		// 返回一级
+		if ($name && strpos($name, '.') === false) {
+			return self::$config[strtolower($name)];
+		}
 		$name = explode('.', $name);
 		$name[0] = strtolower($name[0]);
 		$config = self::$config;
-
 		// 按.拆分成多维数组进行判断
 		foreach ($name as $val) {
 			if (isset($config[$val])) {
@@ -65,7 +56,6 @@ class Config
 				return $default;
 			}
 		}
-
 		return $config;
 	}
 
@@ -79,17 +69,15 @@ class Config
 	{
 		if (is_string($name)) {
 			if (strpos($name, '.') === false) {
-				$name = self::$prefix . '.' . $name;
-			}
-
-			$name = explode('.', $name, 3);
-
-			if (count($name) == 2) {
-				self::$config[strtolower($name[0])][$name[1]] = $value;
+				self::$config[strtolower($name)] = $value;
 			} else {
-				self::$config[strtolower($name[0])][$name[1]][$name[2]] = $value;
+				$name = explode('.', $name, 3);
+				if (count($name) == 2) {
+					self::$config[strtolower($name[0])][$name[1]] = $value;
+				} else {
+					self::$config[strtolower($name[0])][$name[1]][$name[2]] = $value;
+				}
 			}
-
 			return $value;
 		}
 		if (is_array($name)) {
@@ -100,7 +88,6 @@ class Config
 				} else {
 					$result = $name;
 				}
-
 				self::$config[$value] = $result;
 			} else {
 				$result = self::$config = array_merge(self::$config, $name);
@@ -109,7 +96,6 @@ class Config
 			// 为空直接返回 已有配置
 			$result = self::$config;
 		}
-
 		return $result;
 	}
 
@@ -120,13 +106,14 @@ class Config
 	public static function remove($name)
 	{
 		if (strpos($name, '.') === false) {
-			$name = self::$prefix . '.' . $name;
-		}
-		$name = explode('.', $name, 3);
-		if (count($name) == 2) {
-			unset(self::$config[strtolower($name[0])][$name[1]]);
+			unset(self::$config[strtolower($name)]);
 		} else {
-			unset(self::$config[strtolower($name[0])][$name[1]][$name[2]]);
+			$name = explode('.', $name, 3);
+			if (count($name) == 2) {
+				unset(self::$config[strtolower($name[0])][$name[1]]);
+			} else {
+				unset(self::$config[strtolower($name[0])][$name[1]][$name[2]]);
+			}
 		}
 	}
 }
